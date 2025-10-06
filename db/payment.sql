e0a2d45ba82c474e4e637e630ae0173ccbc1d3f9
parsaT79 <parsa.taraj@gmail.com>
2025-10-06 19:17:39 +0300

    ParsaT79 updated

--- db/payment.sql
+++ db/payment.sql
@@ -1,10 +1,11 @@
 create table if not exists payments(
-     id integer primary key autoincrement,
-    document_type text,
+    id integer primary key autoincrement,
     transaction_type text,
+    payment_type text
     date_time text,
     customer_id integer,
     total_amount text,
     items_list text,
+    employee_id integer
     description text
 );
--- db/product.sql
+++ db/product.sql
@@ -1,9 +1,10 @@
 create table if not exists products(
     id integer primary key autoincrement,
-    product_name text,
-    product_type text,
-    expiration_date text,
-    warehouse_code integer,
-    unit_price text,
-    stock_quantity integer
+    name text,
+    brand text,
+    modedl text,
+    serial text,
+    catagory text,
+    unit text,
+    expiration_date text
 );
