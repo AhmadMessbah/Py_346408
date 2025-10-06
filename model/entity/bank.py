8ca41461cf49be3b4fdec306f1eb7ba9b993373e
MelikaArshad <melikaarshad79@gmail.com>
2025-10-06 17:08:36 +0330

    added

--- model/entity/bank.py
+++ model/entity/bank.py
@@ -1,2 +1,11 @@
 class Bank:
-    pass
+    def __init__(self,balance, code, name, account, description, bank_number=None):
+        self.balance = balance
+        self.code = code
+        self.name = name
+        self.account = account
+        self.description = description
+
+
+    def __repr__(self):
+        return f"{self.__dict__}"
--- model/entity/financial_transaction.py
+++ model/entity/financial_transaction.py
@@ -1,2 +1,11 @@
 class FinancialTransaction:
-    pass
+    def __init__(self, customer , employee, amount, date,description,transaction_type):
+        self.customer = customer
+        self.employee = employee
+        self.amount = amount
+        self.date = date
+        self.description = description
+        self.transaction_type = transaction_type
+
+    def __repr__(self):
+        return f"{self.__dict__}"
