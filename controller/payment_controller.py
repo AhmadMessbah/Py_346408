919c439678c63526ef4251148c99f7fc0cc2393a
parsaT79 <parsa.taraj@gmail.com>
2025-10-09 01:08:21 +0300

    ParsaT79 updated

--- controller/payment_controller.py
+++ controller/payment_controller.py
@@ -1,6 +1,18 @@
+from model.entity.payment import Payment
+from model.service.payment_service import PaymentService
+
+
 class PaymentController:
-    def save(self, payment):
-        pass
+    def save(self, transaction_type, payment_type, date_time, customer_id, total_amount, employee_id, description):
+        try:
+            payment = Payment(None, transaction_type, payment_type, date_time, customer_id, total_amount, employee_id,
+                              description)
+            print("zakhire shod, ok")
+            service = PaymentService()
+            service.save(payment)
+            return True, "Saved"
+        except:
+            return False, "Save Error"
 
     def update(self, payment):
         pass
