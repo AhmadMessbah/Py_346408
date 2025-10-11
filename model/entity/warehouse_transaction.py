161e7991ae48101eb0e46b6a19488803ed3dc0e4
Messbah.A <Messbah.A@Gmail.com>
2025-10-11 18:17:12 +0330

    warehouse updated

--- model/entity/warehouse_transaction.py
+++ model/entity/warehouse_transaction.py
@@ -1,15 +1,12 @@
-from datetime import datetime
-
 class WarehouseTransaction:
-    def __init__(self,id,product_name,warehouse_id,quantity,transaction_date,transaction_type,sender,receiver):
+    def __init__(self, id, product_id, quantity, transaction_type, transaction_datetime, customer_id, employee_id):
         self.id = id
-        self.product_name = product_name
-        self.warehouse_id = warehouse_id
+        self.product_id = product_id
         self.quantity = quantity
-        self.transaction_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         self.transaction_type = transaction_type
-        self.sender = sender
-        self.receiver = receiver
+        self.transaction_datetime = transaction_datetime
+        self.customer_id = customer_id
+        self.employee_id = employee_id
 
     def __repr__(self):
-            return f'{self.__dict__}'
+        return f'{self.__dict__}'
