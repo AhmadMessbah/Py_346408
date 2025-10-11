add6a9a10437c2806072343922902a04700b14d1
Messbah.A <Messbah.A@Gmail.com>
2025-10-11 17:58:48 +0330

    file moved to their own folders

--- model/entity/warehouse.py
+++ model/entity/warehouse.py
@@ -1,2 +1,10 @@
+# انبار
 class Warehouse:
-    pass
+        def __init__(self,id,product_name,warehouse_id,quantity):
+                self.id = id
+                self.product_name = product_name
+                self.warehouse_id = warehouse_id
+                self.quantity = quantity
+
+        def __repr__(self):
+                return f'{self.__dict__}'
--- warehouse.sql
+++
@@ -1,6 +0,0 @@
-create table if not exit warehouse_transactions (
-    id integer primary key autoincrement,
-    product_name text,
-    warehouse_id integer,
-    quantity real,
-);
