7c10983781a084051487ca75ef988628a9d0eda2
Parisa-Namvar <pnamvar22@gmail.com>
2025-10-11 19:51:59 +0330

    updated

--- test/order_items_test.py
+++ test/order_items_test.py
@@ -2,8 +2,7 @@ from model.entity.order_items import OrderItems
 from model.service.order_items_service import OrderItemsService
 
 
-order_item = OrderItems(None,None,10,500000,None,
-              None)
+order_item = OrderItems(None,None,10,500000,None,None,"test")
 
 
 order_item1 = OrderItemsService()
