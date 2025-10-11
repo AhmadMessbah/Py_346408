ac00a74780814c937431ce248d55729a9e9d3a75
Parisa-Namvar <pnamvar22@gmail.com>
2025-10-11 19:46:09 +0330

    updated

--- test/order_items_test.py
+++ test/order_items_test.py
@@ -5,7 +5,15 @@ from model.service.order_items_service import OrderItemsService
 order_item = OrderItems(None,None,10,500000,None,
               None)
 
+
 order_item1 = OrderItemsService()
+
 order_item1.save(order_item)
-order_item1.update(order_item)
-order_item1.delete(2)
+
+#order_item1.update(order_item)
+
+#order_item1.delete(2)
+
+#order_item1.find_all()
+
+#order_item1.find_by_id(1)
