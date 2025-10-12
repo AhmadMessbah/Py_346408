44e8c733c818f926f6fae5c7cf8b874aedb36f74
fatemehahang <fatemehahang484@gmail.com>
2025-10-12 21:47:42 +0330

    update

--- view/component/label_with_entry.py
+++ view/component/label_with_entry.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+
 
 class LabelWithEntry:
     def __init__(self, master, label_text, x, y, distance=90, data_type=IntVar):
@@ -22,4 +24,20 @@ class LabelWithEntry:
             self.variable.set(0.0)
         elif self.data_type== BooleanVar:
             self.variable.set(True)
+
+class Table:
+    def __init__(self, parent):
+        self.table = ttk.Treeview(parent, columns=[1, 2, 3, 4, 5, 6, 7], show="headings")
+        self.table.place(x=300,y=20)
+        self.set_column_headings()
+        self.set_column_widths()
+
+    def set_column_headings(self):
+        headings = ["ID", "Order ID", "Product ID", "Quantity", "Price", "Discount", "Description"]
+        for i, heading in enumerate(headings):
+            self.table.heading(i, text=heading)
+    def set_column_widths(self):
+        widths = [40, 60, 70, 60, 90, 60, 140]
+        for i, width in enumerate(widths):
+            self.table.column(i, width=width)
 
