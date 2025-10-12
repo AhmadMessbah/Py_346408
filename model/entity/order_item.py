3ce31352d01da70aa92610572cf6d73cc95a6075
fbakhti-blip <fbakhti@gmail.com>
2025-10-12 05:11:11 +0330

    "s" dropped

--- model/entity/order_item.py
+++ model/entity/order_item.py
@@ -1,7 +1,8 @@
 class OrderItem:
 
-    def __init__(self, id, product_id, quantity, price, discount=None, description=None):
+    def __init__(self, id, order_id, product_id, quantity, price, discount=None, description=None):
         self.id = id
+        self.order_id = order_id
         self.product_id = product_id
         self.quantity = quantity
         self.price = price
