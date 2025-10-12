b0d06a2fd4b8c7eeb1922ba76ae2dcac03c043df
fbakhti-blip <fbakhti@gmail.com>
2025-10-12 07:56:32 +0330

    Order Item View Initial Commit

--- view/order_item_view.py
+++ view/order_item_view.py
@@ -33,46 +33,41 @@ win.title("Order Item View")
 win.geometry("900x400")
 
 
-# ID
-id = LabelWithEntry(win, "ID:", 20,20)
-
 # Order ID
-order_id = LabelWithEntry(win, "Order ID:", 20,60)
+order_id = LabelWithEntry(win, "Order ID:", 20,20)
 
 # Product ID
-product_id = LabelWithEntry(win, "Product ID:", 20,100)
+product_id = LabelWithEntry(win, "Product ID:", 20,60)
 
 # Quantity
-quantity = LabelWithEntry(win, "Quantity:", 20,140)
+quantity = LabelWithEntry(win, "Quantity:", 20,100)
 
 # Price
-price = LabelWithEntry(win, "Price:", 20,180)
+price = LabelWithEntry(win, "Price:", 20,140)
 
 # Discount
-discount = LabelWithEntry(win, "Discount:", 20,220)
+discount = LabelWithEntry(win, "Discount:", 20,180)
 
 # Description
-description = LabelWithEntry(win, "Description:", 20,260, data_type=StringVar)
+description = LabelWithEntry(win, "Description:", 20,220, data_type=StringVar)
 
 # Table
-table = ttk.Treeview(win,columns=[1,2,3,4,5,6,7],show="headings")
+table = ttk.Treeview(win,columns=[1,2,3,4,5,6],show="headings")
 table.place(x=300,y=20)
 
-table.heading(1,text="ID")
-table.heading(2,text="Order ID")
-table.heading(3,text="Product ID")
-table.heading(4,text="Quantity")
-table.heading(5,text="Price")
-table.heading(6,text="Discount")
-table.heading(7,text="Description")
+table.heading(1,text="Order ID")
+table.heading(2,text="Product ID")
+table.heading(3,text="Quantity")
+table.heading(4,text="Price")
+table.heading(5,text="Discount")
+table.heading(6,text="Description")
 
-table.column(1, width=40)
-table.column(2, width=60)
-table.column(3, width=70)
-table.column(4, width=60)
-table.column(5, width=90)
-table.column(6, width=60)
-table.column(7, width=140)
+table.column(1, width=60)
+table.column(2, width=70)
+table.column(3, width=60)
+table.column(4, width=90)
+table.column(5, width=60)
+table.column(6, width=140)
 
 Button(win, text="Save", width=12, command=save_click).place(x=80,y=300)
 
