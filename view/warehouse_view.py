7f550e07161b365f47d5bb9eb4980cd3f2accd94
Ali Zabihian <zabihianali95@gmail.com>
2025-10-13 18:07:55 +0330

    .

--- view/warehouse_view.py
+++ view/warehouse_view.py
@@ -4,6 +4,21 @@ root = Tk()
 root.title("Warehouse View")
 root.geometry("300x250")
 
+
+def edit():
+    win = Tk()
+    win.title("Edit")
+    win.geometry("200x200")
+    l = Label(win, text="enter product id")
+    l.pack()
+    entry = Entry(win)
+    entry.pack()
+    sub = Button(win, text="submit")
+    sub.pack()
+
+
+
+
 # ADD-SECTION
 add_label = Label(root, text="Add product to warehouse")
 add_label.pack()
@@ -13,7 +28,7 @@ add_btn.pack(pady=5)
 # EDIT-SECTION
 edit_label = Label(root, text="Edit product in warehouse")
 edit_label.pack(pady=10)
-edit_btn = Button(root, text="Edit product", bg="yellow")
+edit_btn = Button(root, text="Edit product", bg="yellow", command=edit)
 edit_btn.pack(pady=5)
 
 # FIND-SECTION
