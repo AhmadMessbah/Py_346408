05e15a0273e13488244ac8b1459cdd44529c6023
arminyarmohammadi <arminsnow2025@gmail.com>
2025-10-13 20:22:52 +0330

    updated armin

--- controller/order_controller.py
+++ controller/order_controller.py
@@ -3,7 +3,6 @@ from model.service.order_service import OrderService
 
 
 class OrderController:
-
     def save(self, order_type, customer_id, employee_id, order_item_list, date_time,
                  payment_id, warehouse_transaction_id, tax=None, total_discount=None,
                  total_amount=None):
@@ -11,21 +10,45 @@ class OrderController:
             order = Order(None, order_type, customer_id, employee_id, order_item_list, date_time,
                  payment_id, warehouse_transaction_id, tax=None, total_discount=None,
                  total_amount=None)
-            print("Save Requested, ok")
             service = OrderService()
             service.save(order)
-            return True, "Saved"
+            return True, f"order Saved Successfully \n{order}"
         except:
             return False, "Save Error"
 
-    def update(self, order):
-        pass
+    def update(self, id,order_type, customer_id, employee_id, order_item_list, date_time,
+                 payment_id, warehouse_transaction_id, tax=None, total_discount=None,
+                 total_amount=None):
+        try:
+            order = Order(id, order_type, customer_id,employee_id, order_item_list, date_time,
+                 payment_id, warehouse_transaction_id, tax=None, total_discount=None,
+                 total_amount=None)
+            service = OrderService()
+            service.update(order)
+            return True, f"order Updated Successfully \n{order}"
+        except:
+            return False, "Update Error"
 
     def delete(self, id):
-        pass
+        try:
+            order = OrderService()
+            order.delete(id)
+            return True, f"order with Id {id} delete successfully"
+        except:
+            return False, "Delete Error"
 
     def find_all(self):
-        pass
+        try:
+            service = OrderService()
+            order_list = service.find_all()
+            return True, order_list
+        except:
+            return False, "Find All Error"
 
     def find_by_id(self, id):
-        pass
+        try:
+            service = OrderService()
+            order = service.find_by_id(id)
+            return True, order
+        except:
+            return False, "Find By Id Error"
