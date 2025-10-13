8fb77bbd2202abd4b26fdf63d19ef1dcf9d825d5
fbakhti-blip <fbakhti@gmail.com>
2025-10-13 04:31:14 +0330

    View Debugged

---
+++ view/component/win_table.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import Treeview
+
+class LabelWithEntry:
+    def __init__(self, master, label_text, x, y, distance=90, data_type=IntVar):
+        self.data_type = data_type
+        self.variable = data_type()
+        Label(master, text=label_text).place(x=x, y=y)
+        Entry(master, textvariable=self.variable).place(x=x + distance, y=y)
+
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
+
+    def clear(self):
+        if self.data_type == IntVar:
+            self.variable.set(0)
+        elif self.data_type == StringVar:
+            self.variable.set("")
+        elif self.data_type == DoubleVar:
+            self.variable.set(0.0)
+        elif self.data_type== BooleanVar:
+            self.variable.set(True)
+
+
+class HeadingWithWidth:
+    def __init__(self, table, column_id, label_text, width=60):
+        Treeview.heading(table, column=column_id, text=label_text)
+        Treeview.column(table, column=column_id, width=width)
+
+
+class Table:
+    def __init__(self, parent):
+        self.table = ttk.Treeview(parent, columns=[1, 2, 3, 4, 5, 6, 7], show="headings")
+        self.table.place(x=300,y=20)
+        self.set_column_headings()
+        self.set_column_widths()
+
+    def set_column_headings(self):
+        headings = ["ID", "Order ID", "Product ID", "Quantity", "Price", "Discount", "Description"]
+        for i, heading in enumerate(headings):
+            self.table.heading(i, text=heading)
+
+    def set_column_widths(self):
+        widths = [40, 60, 70, 60, 90, 60, 140]
+        for i, width in enumerate(widths):
+            self.table.column(i, width=width)
