723de338b69058d4fdda968e5665ac967f0d6a8e
fatemehahang <fatemehahang484@gmail.com>
2025-10-13 18:34:34 +0330

    update

--- view/order_item_view.py
+++ view/order_item_view.py
@@ -1,81 +1,48 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox
-from view.component.heading_with_width import HeadingWithWidth
 from view.component.lable_with_entry import LabelWithEntry
 
-def reset_form():
-    id.clear()
-    order_id.clear()
-    product_id.clear()
-    quantity.clear()
-    price.clear()
-    discount.clear()
-    description.clear()
-
-def save_click():
-    order_item = (id.get(), order_id.get(), product_id.get(), quantity.get(),
-                  price.get(), discount.get(), description.get())
-
-# todo *** should not create object ?? *** used tuple instead object ***
-
-    table.insert(
-        "",
-        END,
-        values=order_item
-    )
-
-    messagebox.showinfo("Save", "Order item has been saved")
-    reset_form()
-
-# todo *** database connection / save object
+window = Tk()
+window.title("Order Item")
+window.geometry("900x400")
 
 
-# todo *** complete update_click ***
-def update_click():
-    pass
+id = LabelWithEntry(window, "ID", 20,20, data_type=IntVar)
 
-win = Tk()
-win.title("Order Item View")
-win.geometry("900x400")
+order_id = LabelWithEntry(window, "Order ID", 20,60)
 
-# ID
-id = LabelWithEntry(win, "ID:", 20,20)
+product_id = LabelWithEntry(window, "Product ID", 20,100)
 
-# Order ID
-order_id = LabelWithEntry(win, "Order ID:", 20,60)
+quantity = LabelWithEntry(window, "Quantity", 20,140)
 
-# Product ID
-product_id = LabelWithEntry(win, "Product ID:", 20,100)
+price = LabelWithEntry(window, "Price", 20,180)
 
-# Quantity
-quantity = LabelWithEntry(win, "Quantity:", 20,140)
+discount = LabelWithEntry(window, "Discount", 20,220)
 
-# Price
-price = LabelWithEntry(win, "Price:", 20,180)
-
-# Discount
-discount = LabelWithEntry(win, "Discount:", 20,220)
-
-# Description
-description = LabelWithEntry(win, "Description:", 20,260, data_type=StringVar)
+description = LabelWithEntry(window, "Description", 20,260)
 
 
-# Table
-table = ttk.Treeview(win,columns=[1,2,3,4,5,6,7],show="headings")
+table = ttk.Treeview(window,columns=[1,2,3,4,5,6,7],show="headings", height=14)
 table.place(x=300,y=20)
 
-table_id = HeadingWithWidth(table,1, "ID", 40)
-table_order_id = HeadingWithWidth(table,2, "Order ID")
-table_product_id = HeadingWithWidth(table,3, "Product ID", 70)
-table_quantity = HeadingWithWidth(table,4, "Quantity")
-table_price = HeadingWithWidth(table,5, "Price", 90)
-table_discount = HeadingWithWidth(table,6, "Discount")
-table_description = HeadingWithWidth(table,7, "Description", 140)
+table.heading(1, text="ID")
+table.heading(2, text="Order ID")
+table.heading(3, text="Product ID")
+table.heading(4, text="Quantity")
+table.heading(5, text="Price")
+table.heading(6, text="Discount")
+table.heading(7, text="Description")
 
+table.column(1, width=40)
+table.column(2, width=60)
+table.column(3, width=70)
+table.column(4, width=60)
+table.column(5, width=90)
+table.column(6, width=60)
+table.column(7, width=140)
 
-Button(win, text="Save", width=12, command=save_click).place(x=80,y=300)
-Button(win, text="Update", width=12, command=update_click).place(x=80,y=340)
+Button(window, text="Save", width=7).place(x=20,y=300)
+Button(window, text="Edit", width=7).place(x=100,y=300)
+Button(window, text="Delete", width=7).place(x=180,y=300)
 
-
-win.mainloop()
+window.mainloop()
