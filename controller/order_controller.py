9b3d38ab0930c94f34919e4b9157ac6e7712761e
f.mehri <fatim127913@gmail.com>
2025-10-16 19:30:58 +0330

    debugged

--- controller/order_controller.py
+++ controller/order_controller.py
@@ -3,38 +3,41 @@ from model.service.order_service import OrderService
 
 
 class OrderController:
-    def save(self, order_type, customer_id, employee_id, order_item_list, date_time,
-                 payment_id, warehouse_transaction_id, tax=None, total_discount=None,
-                 total_amount=None):
+    def save(self, order_type, customer_id, employee_id, date_time,
+                 payment_id, warehouse_transaction_id, tax, total_discount,
+                 total_amount):
         try:
-            order = Order(None, order_type, customer_id, employee_id, order_item_list, date_time,
-                 payment_id, warehouse_transaction_id, tax=None, total_discount=None,
-                 total_amount=None)
+            order = Order(None, order_type, customer_id, employee_id, date_time,
+                 payment_id, warehouse_transaction_id, tax, total_discount,
+                 total_amount)
             service = OrderService()
             service.save(order)
-            return True, f"order Saved Successfully \n{order}"
-        except:
+            return True, f"Order Saved Successfully \n{order}"
+        except Exception as e:
+            print("Save Error",e)
             return False, "Save Error"
 
-    def update(self, id,order_type, customer_id, employee_id, order_item_list, date_time,
-                 payment_id, warehouse_transaction_id, tax=None, total_discount=None,
-                 total_amount=None):
+    def update(self, id,order_type, customer_id, employee_id,  date_time,
+                 payment_id, warehouse_transaction_id, tax, total_discount,
+                 total_amount):
         try:
-            order = Order(id, order_type, customer_id,employee_id, order_item_list, date_time,
-                 payment_id, warehouse_transaction_id, tax=None, total_discount=None,
-                 total_amount=None)
+            order = Order(id, order_type, customer_id,employee_id, date_time,
+                 payment_id, warehouse_transaction_id, tax, total_discount,
+                 total_amount)
             service = OrderService()
             service.update(order)
-            return True, f"order Updated Successfully \n{order}"
-        except:
+            return True, f"Order Updated Successfully \n{order}"
+        except Exception as e:
+            print("Update Error",e)
             return False, "Update Error"
 
     def delete(self, id):
         try:
             order = OrderService()
             order.delete(id)
-            return True, f"order with Id {id} delete successfully"
-        except:
+            return True, f"Order with Id {id} delete successfully"
+        except Exception as e:
+            print("Delete Error",e)
             return False, "Delete Error"
 
     def find_all(self):
@@ -42,7 +45,8 @@ class OrderController:
             service = OrderService()
             order_list = service.find_all()
             return True, order_list
-        except:
+        except Exception as e:
+            print("Find All Error",e)
             return False, "Find All Error"
 
     def find_by_id(self, id):
@@ -50,5 +54,6 @@ class OrderController:
             service = OrderService()
             order = service.find_by_id(id)
             return True, order
-        except:
+        except Exception as e:
+            print("Find By Id Error",e)
             return False, "Find By Id Error"
