7e954e6a44584b7191ad30cb49ca987c71c7b865
Lion <safarpourromina@gmail.com>
2025-10-17 22:06:02 +0330

    warehouse_view commited

--- view/warehouse_view.py
+++ view/warehouse_view.py
@@ -1,36 +1,99 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import IntVar
+import tkinter.messagebox as messagebox
+from tkinter.ttk import Button
+
+from controller.warehouse_controller import WarehouseController
 from view.component.lable_with_entry import LabelWithEntry
+class WarehouseView:
+    def __init__(self):
+        self.warehouse_controller = WarehouseController()
+        self.window=tk()
+        self.window.title("Warehouse View")
+        self.window.geometry("750x320")
 
-window = Tk()
-window.geometry("750x320")
-window.title("Warehouse View")
-
-id= LabelWithEntry(window, "ID", 20,20, data_type=IntVar)
-product_id= LabelWithEntry(window, "Product_id", 20,60)
-quantity= LabelWithEntry(window, "Quantity", 20,100)
-
-
-
-table = ttk.Treeview(window,columns=[1,2,3],show="headings", height=12)
-table.place(x=270,y=20)
-
-table.heading(1, text="ID")
-table.heading(2, text="Product_ID")
-table.heading(3, text="Quantity")
+        self.id = LabelWithEntry(self.window,"Id",20,20, data_type=IntVar,state="readonly")
+        self.product_id = LabelWithEntry(self.window,"ProductId",20,60)
+        self.quantity = LabelWithEntry(self.window,"Quantity",20,100)
 
 
+        self.table = ttk.Treeview(self.window,columns=[1,2,3],show="headings", height=16)
+        self.table.place(x=270,y=20)
 
-table.column(1, width=30)
-table.column(2, width=100)
-table.column(3, width=100)
+        self.table.heading(1, text="Id")
+        self.table.heading(2, text="FirstName")
+        self.table.heading(3, text="LastName")
+
+
+        self.table.column(1, width=40)
+        self.table.column(2, width=100)
+        self.table.column(3, width=100)
+
+
+        self.table.bind("<<TreeviewSelect>>", self.select_from_table)
+
+        Button(self.window, text="Save", width=8, command=self.save_click).place(x=20, y=340)
+        Button(self.window, text="Edit", width=8, command=self.save_click).place(x=100, y=340)
+        Button(self.window, text="Delete", width=8, command=self.save_click).place(x=180, y=340)
+        self.reset_form()
+        self.window.mainloop()
+
+
+    def save_click(self):
+        status, message = self.warehouse_controller.save(self.product_id.get(), self.quantity.get())
+        if status:
+            messagebox.showinfo("Warehouse Save", message)
+            self.reset_form()
+        else:
+            messagebox.showerror("Warehouse Save Error", message)
+
+
+    def edit_click(self):
+        status, message = self.warehouse_controller.update(self.id.get(), self.product_id.get(), self.quantity.get())
+        if status:
+            messagebox.showinfo("Warehouse update", message)
+            self.reset_form()
+        else:
+            messagebox.showerror("Warehouse update Error", message)
+
+    def delete_click(self):
+        status, message = self.warehouse_controller.delete(self.id.get())
+        if status:
+            messagebox.showinfo("Warehouse Delete", message)
+            self.reset_form()
+        else:
+            messagebox.showerror("Warehouse Delete Error", message)
+
+    def reset_form(self):
+        self.id.clear()
+        self.product_id.clear()
+        self.quantity.clear()
+
+        status, warehouse_list = self.warehouse_controller.find_all()
+        self.refresh_table(warehouse_list)
+
+    def refresh_table(self, warehouse_list):
+        for item in self.table.get_children():
+            self.table.delete(item)
+
+        for employee in warehouse_list:
+            warehouse_tuple = tuple(warehouse_list.__dict__.values())
+            self.table.insert("", END, values=warehouse_tuple)
+
+    def select_from_table(self, event):
+                selected_warehouse = self.table.item(self.table.focus())["values"]
+                if selected_warehouse:
+                    warehouse = WarehouseView(*selected_warehouse)
+                    self.id.set(warehouse.id)
+                    self.product_id.set(warehouse.product_id)
+                    self.quantity.set(warehouse.quantity)
 
 
 
-Button(window, text="Save", width=7).place(x=20,y=260)
-Button(window, text="Edit", width=7).place(x=100,y=260)
-Button(window, text="Delete", width=7).place(x=180, y=260)
 
-window.mainloop()
+
+
+
 
 
