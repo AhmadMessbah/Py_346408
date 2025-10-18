3013eac56073cdedfa309d48ae743f7ca3ec248e
Messbah.A <Messbah.A@Gmail.com>
2025-10-18 18:12:11 +0330

    employee debugged

---
+++ app.py
@@ -0,0 +1,10 @@
+from view.employee_view import EmployeeView
+
+# Phase 1 Passed
+# ui = CustomerView()
+
+ui = EmployeeView()
+
+
+
+# Phase 1 Not Passed
--- view/__init__.py
+++ view/__init__.py
@@ -2,3 +2,5 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from view.component.lable_with_entry import LabelWithEntry
+
+from view.customer_view import CustomerView
