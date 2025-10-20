5cc30661d3f1994d914c34aef2e0cff770ade779
Zhoubin <zhobinghahvarehchi@gmail.com>
2025-10-20 19:01:23 +0330

    updated

--- view/delivery_view.py
+++ view/delivery_view.py
@@ -16,22 +16,14 @@ class DeliveryView:
         self.address = LabelWithEntry(self.window, "Address", 20, 140)
         self.description = LabelWithEntry(self.window, "Description", 20, 180)
 
-        self.table = ttk.Treeview(self.window, columns=[1, 2, 3, 4, 5], show="headings", height=12)
-        self.table.place(x=270, y=20)
-
-        self.table.heading(1, text="Id")
-        self.table.heading(2, text="FirstName")
-        self.table.heading(3, text="LastName")
-        self.table.heading(4, text="Address")
-        self.table.heading(5, text="Description")
-
-        self.table.column(1, width=40)
-        self.table.column(2, width=100)
-        self.table.column(3, width=100)
-        self.table.column(4, width=100)
-        self.table.column(5, width=100)
-
-        self.table.bind("<<TreeviewSelect>>", self.select_from_table)
+        self.table = Table(
+            self.window,
+            ["Id", "first_name", "last_name", "address", "description"],
+            [40, 100, 100, 100, 100],
+            275, 20,
+            12,
+            self.select_from_table
+        )
 
         Button(self.window, text="Save", width=7, command=self.save_click).place(x=20, y=260)
         Button(self.window, text="Edit", width=7, command=self.edit_click).place(x=100, y=260)
