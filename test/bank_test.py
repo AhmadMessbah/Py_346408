de66c431e0dcfe3e16b54fa76986bc96efa48c03
Messbah.A <Messbah.A@Gmail.com>
2025-10-20 19:28:02 +0330

    service tasks

--- test/bank_test.py
+++ test/bank_test.py
@@ -1,9 +1,21 @@
 from model.entity.bank import Bank
 from model.service.bank_service import BankService
 from view.bank_view import *
-bank = Bank(1, "saderat", "15000", 2500, "tozih jadid")
 
 service = BankService()
+
+# bank1 = Bank(1, "saderat", "saving", 2500, "tozih jadid")
+# service.save(bank1)
+#
+bank2 = Bank(2, "mellat", "card", 2500, "tozih jadid")
+service.save(bank2)
+
+# bank3 = Bank(3, "melli", "card", 2500, "tozih jadid")
+# service.save(bank3)
+
+print(service.find_by_name("mellat"))
+# print(service.find_by_account("saving2"))
+
 
 # test passed
 # service.save(bank)
