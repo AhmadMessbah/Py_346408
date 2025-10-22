12bcba92be1999d7895e2b1668df0a1f35487346
taranom <trnmbghry2gmail.com>
2025-10-22 11:46:46 -0700

    .

--- tools/employee_validator.py
+++ tools/employee_validator.py
@@ -2,28 +2,28 @@ import re
 
 
 def first_name_validator(first_name):
-    if not (type(first_name) == str and re.match(r"^\d{1,3}(,\d{3})*$", first_name)):
+    if not (type(first_name) == str and re.match(r"^[a-zA-Z\s]{2,20}$", first_name)):
         raise ValueError("Invalid first_name !!!")
     else:
         return first_name
 
 
 def last_name_validator(last_name):
-    if not (type(last_name) == str and re.match(r"^\d{1,3}(,\d{3})*$", last_name)):
+    if not (type(last_name) == str and re.match(r"^[a-zA-Z\s]{2,20}$", last_name)):
         raise ValueError("Invalid last_name !!!")
     else:
         return last_name
 
 
 def salary_validator(salary):
-    if not (type(salary) == str and re.match(r"^\d{1,3}(,\d{3})*$", salary)):
+    if not (type(salary) == int and salary>0, salary):
         raise ValueError("Invalid first_name !!!")
     else:
         return salary
 
 
 def phone_number_validator(phone_number):
-    if not (type(phone_number) == str and re.match(r"^\d{1,3}(,\d{3})*$", phone_number)):
+    if not (type(phone_number) == int and re.match(r"^\\+?[1-9][0-9]{7,14}$", phone_number)):
         raise ValueError("Invalid first_name !!!")
     else:
         return phone_number
