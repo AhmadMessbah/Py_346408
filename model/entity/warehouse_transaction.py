d30e933a3802bbd665ab954d4e6acba810dab0e4
Ali Zabihian <zabihianali95@gmail.com>
2025-10-24 11:46:56 +0330

    changes made to controller and transaction

--- model/entity/warehouse_transaction.py
+++ model/entity/warehouse_transaction.py
@@ -1,3 +1,5 @@
+from tools.warehouse_transaction_validator import *
+
 class WarehouseTransaction:
     def __init__(self, id, product_id, quantity, transaction_type, transaction_datetime, customer_id, employee_id):
         self.id = id
@@ -7,6 +9,14 @@ class WarehouseTransaction:
         self.transaction_datetime = transaction_datetime
         self.customer_id = customer_id
         self.employee_id = employee_id
+
+    def validate(self):
+        product_id_validator(self.id)
+        quantity_validator(self.quantity)
+        transaction_type_validator(self.transaction_type)
+        datetime_validator(self.transaction_datetime)
+        customer_id_validator(self.customer_id)
+        employee_id_validator(self.employee_id)
 
     def __repr__(self):
         return f'{self.__dict__}'
