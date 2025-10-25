f176805025790f568f6b237efb09a68f4400dc05
taranom <trnmbghry2gmail.com>
2025-10-24 22:03:11 -0700

    .

--- test/payment_test.py
+++ test/payment_test.py
@@ -1,20 +1,39 @@
 from model.entity.payment import Payment
 from model.service.payment_service import PaymentService
 
-payment = Payment(1, "receive", "cash", "2025-02-02", 12,200,11,"daryaftshod")
+payment1 = Payment(1, "receive", "cash", "2025-02-02", 12,200,11,"daryaftshod")
+
+payment2= Payment(2, "pay", "cart", "2025-04-02", 22,250,11,"pardakhtshod")
+
 
 service = PaymentService()
 
 #test passed
-#service.save(payment)
+#service.save(payment1)
+
+#test passed????
+#service.update(payment1)
 
 #test passed
-#service.update(payment)
-
-#test passed
-#service.delete(1)
+#service.delete(5)
 
 #test passed
 #print(service.find_all())
 
-#print(service.find_by_id(2))
+#test passed
+#print(service.find_by_id(6))
+
+#test did not passed
+#print(service.find_by_transaction_type("receive"))
+
+#test  did not passed
+#print(service.find_by_payment_type("cash"))
+
+#test   did not passed
+#print(service.find_by_date_time_range("2025-02-02"))
+
+#test  did not passed
+#print(service.find_by_date_time_range_and_customer_id(""))
+
+#test passed
+#print(service.find_by_date_time_range_and_employee_id(""))
