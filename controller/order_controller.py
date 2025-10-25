2dd1a1973dd57408073b66f54af03b890f8c538b
pnamv <pnamvar22@gmail.com>
2025-10-25 04:08:52 -0700

    updated

--- controller/order_controller.py
+++ controller/order_controller.py
@@ -41,56 +41,49 @@ class OrderController:
     def find_all(self):
         try:
             service = OrderService()
-            order_list = service.find_all()
-            return True, order_list
+            return True, service.find_all()
         except Exception as e:            
             return False, e
 
     def find_by_id(self, id):
         try:
             service = OrderService()
-            order_list = service.find_by_id(id)
-            return True, order_list
+            return True, service.find_by_id(id)
         except Exception as e:            
             return False, e
         
     def find_by_order_type(self, order_type):
         try:
             service = OrderService()
-            order_list = service.find_by_order_type(order_type)
-            return True, order_list
+            return True, service.find_by_order_type(order_type)
         except Exception as e:
             return False, e
         
     def find_by_customer_id(self, customer_id):
         try:
             service = OrderService()
-            order_list = service.find_by_customer_id(customer_id)
-            return True, order_list
+            return True, service.find_by_customer_id(customer_id)
         except Exception as e:            
             return False, e
         
     def find_by_employee_id(self, employee_id):
         try:
             service = OrderService()
-            order_list = service.find_by_employee_id(employee_id)
-            return True, order_list
+            return True, service.find_by_employee_id(employee_id)
         except Exception as e:            
             return False, e
         
     def find_by_date_time_range(self, start_date_time, end_date_time):
         try:
             service = OrderService()
-            order_list = service.find_by_date_time_range(start_date_time, end_date_time)
-            return True, order_list
+            return True, service.find_by_date_time_range(start_date_time, end_date_time)
         except Exception as e:            
             return False, e
         
     def find_by_date_time_range_and_customer_id(self, start_date_time, end_date_time, customer_id):
         try:
             service = OrderService()
-            order_list = service.find_by_date_time_range_and_customer_id(start_date_time, end_date_time, customer_id)
-            return True, order_list
+            return True, service.find_by_date_time_range_and_customer_id(start_date_time, end_date_time, customer_id)
         except Exception as e:            
             return False, e
     
