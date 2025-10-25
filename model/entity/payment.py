650530eabe91db1d0dd91a84c3cc8b13c48eae38
parsaT79 <parsa.taraj@gmail.com>
2025-10-25 17:12:16 +0300

    ParsaT79 updated

--- model/entity/payment.py
+++ model/entity/payment.py
@@ -1,5 +1,9 @@
+from tools.payment_validator import *
+
+
 class Payment:
-    def __init__(self,id,transaction_type,payment_type,date_time,customer_id,total_amount,employee_id,description):
+    def __init__(self, id, transaction_type, payment_type, date_time, customer_id, total_amount, employee_id,
+                 description):
         self.id = id
         self.transaction_type = transaction_type
         self.payment_type = payment_type
@@ -9,9 +13,18 @@ class Payment:
         self.employee_id = employee_id
         self.description = description
 
+    def validate(self):
+        transaction_type_validator(self.transaction_type)
+        payment_type_validator(self.payment_type)
+        date_time_validator(self.date_time)
+        customer_id_validator(self.customer_id)
+        total_amount_validator(self.total_amount)
+        employee_id_validator(self.employee_id)
+        description_validator(self.description)
+
     def __repr__(self):
         return f"{self.__dict__}"
 
     def to_tuple(self):
         return tuple((self.id, self.transaction_type, self.payment_type,
-             self.date_time, self.customer_id, self.total_amount, self.employee_id, self.description))
+                      self.date_time, self.customer_id, self.total_amount, self.employee_id, self.description))
